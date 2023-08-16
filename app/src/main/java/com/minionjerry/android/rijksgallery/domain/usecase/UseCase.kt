1f6604a329c77527967c2b9cca8561c1ba6bd0a4
Kany Kurbanova <kanykei.nur@gmail.com>
2023-08-16 16:18:37 +0200

    Add DTO's abstraction

---
+++ app/src/main/java/com/minionjerry/android/rijksgallery/domain/usecase/UseCase.kt
@@ -0,0 +1,30 @@
+package com.minionjerry.android.rijksgallery.domain.usecase
+
+import com.minionjerry.android.rijksgallery.domain.entity.Result
+import com.minionjerry.android.rijksgallery.domain.entity.UseCaseException
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.processNextEventInCurrentThread
+
+abstract class UseCase<I: UseCase.Request, O: UseCase.Response>(private val configuration: Configuration) {
+
+    fun execute(request: I) = process(request)
+        .map {
+            Result.Success(it) as Result<O>
+        }
+        .flowOn(configuration.dispatcher)
+        .catch {
+            emit(Result.Error(UseCaseException.createFromThrowable(it)))
+        }
+
+
+    abstract fun process(request: I): Flow<Response>
+
+    interface Request
+    interface Response
+    class Configuration(val dispatcher: CoroutineDispatcher)
+
+}

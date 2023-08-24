ee45561b0171dbf7d703e2955368fed9b40121f5
Kany Kurbanova <kanykei.nur@gmail.com>
2023-08-24 16:23:02 +0200

    Display paging loading state

--- app/src/main/java/com/minionjerry/android/rijksgallery/presentation/artobject/list/ArtObjectListScreen.kt
+++ app/src/main/java/com/minionjerry/android/rijksgallery/presentation/artobject/list/ArtObjectListScreen.kt
@@ -5,13 +5,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,8 +25,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
@@ -31,13 +39,70 @@ import coil.size.Size
 import com.minionjerry.android.rijksgallery.R
 import com.minionjerry.android.rijksgallery.presentation.navigation.NavRoutes
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ArtObjectListScreen(viewModel: ArtObjectListViewModel, navController: NavController) {
     viewModel.loadArtObjects()
     val artObjects = viewModel.artObjectListFlow.collectAsLazyPagingItems()
-    ArtObjectList(artObjects = artObjects) {
-        navController.navigate(NavRoutes.ArtObject.routeForArtObject(it))
+
+    Scaffold(
+        topBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .background(MaterialTheme.colorScheme.primary),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    modifier = Modifier
+                        .padding(vertical = 16.dp, horizontal = 16.dp)
+                        .weight(1.0f),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            ArtObjectList(artObjects = artObjects) {
+                navController.navigate(NavRoutes.ArtObject.routeForArtObject(it))
+            }
+        }
+            LazyColumn(
+                modifier = Modifier.padding(it)
+            ) {
+                artObjects.apply {
+                    when {
+                        loadState.refresh is LoadState.Loading -> {
+                            item { Loading() }
+                        }
+
+                        loadState.refresh is LoadState.Error -> {
+                            val error = artObjects.loadState.refresh as LoadState.Error
+                            item {
+                                Error(error.error.localizedMessage!!)
+                            }
+                        }
+
+                        loadState.append is LoadState.Loading -> {
+                            item { Loading() }
+                        }
+
+                        loadState.append is LoadState.Error -> {
+                            val error = artObjects.loadState.append as LoadState.Error
+                            item {
+                                Error(errorMessage = error.error.localizedMessage!!)
+                            }
+                        }
+                    }
+                }
+
+        }
     }
+
+
 }
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -51,7 +116,11 @@ fun ArtObjectList(artObjects: LazyPagingItems<ArtObjectUiModel>, onGridClick: (S
             items(artObjects.itemCount) { index ->
                 val data = artObjects[index]!!
                 when (data) {
-                    is ArtObjectListItemModel -> ArtObjectImage(artObj = data, onCardClick = onGridClick)
+                    is ArtObjectListItemModel -> ArtObjectImage(
+                        artObj = data,
+                        onCardClick = onGridClick
+                    )
+
                     is HeaderItemModel ->
                         ArtistHeader(artist = data.artist, color = Color.Black)
                 }
